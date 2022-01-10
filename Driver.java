9dd1a126bee26200631ef4ba7642488b50b38826
Neil Lin <nlin30@stuy.edu>
2022-01-10 13:44:36 -0500

    push

---
+++ Driver.java
@@ -0,0 +1,10 @@
+public class Driver{
+
+  public static void main (Strings [] args){
+    ArrayList coco = populate( 10, 1, 1000 );
+    System.out.println( "ArrayList coco before sorting:\n" + coco );
+    selectionSortV(coco);
+    System.out.println( "ArrayList coco after sorting:\n" + coco );
+  }
+
+}
--- Sorts.java
+++ Sorts.java
@@ -21,9 +21,9 @@ public class Sorts{
      al.set( i, al.set( randomIndex, al.get(i) ) );
    }
  }
- 
+
   public static ArrayList<Comparable> bubble (ArrayList<Comparable> input )  {
-    
+    numComparisons = 0; numSwaps = 0;
     ArrayList<Comparable> data = input;
 
     boolean isSorted = true;
@@ -31,23 +31,28 @@ public class Sorts{
     int temp;
     while(!isSorted){
       for (int j = size - 1; j > 0; j--) {
+        numComparisons++; // Each time a for loop is run, there's a comparison being made
         //swapping
         isSorted = true;
         if (data.get(j).compareTo(data.get(j - 1)) < 0) {
-          isSorted = false; 
+          isSorted = false;
           temp = data.get(j);
           data.set(j, data.get(j - 1));
           data.set(j - 1, temp);
-          
+          numSwaps++;
         }
-        
+        numComparisons++; // data.get(j).compareTo(data.get(j - 1)) < 0 is a comparison
       }
-      
+
     }
-  return data;  
+    System.out.println("# of Comparisons: " + numComparisons);
+    System.out.println("# of Swaps: " + numSwaps);
+  return data;
+
   }
 
   public static ArrayList<Comparable> selection( ArrayList<Comparable> input) {
+    numComparisons = 0; numSwaps = 0;
     ArrayList<Comparable> data = input;
 
     //maxPos will point to position of SELECTION (greatest value)
@@ -55,14 +60,14 @@ public class Sorts{
 
     numComparisons = 0; numSwaps = 0;
     for(int pass = data.size(); pass > 0; pass--) {
-      numComparisons++;
+      numComparisons++;   // Each time a for loop is run, there's a comparison being made
       System.out.println( "\nbegin pass " + (data.size()-pass) );//diag
 
       //check for greatest
-      
+
       for(int index = 0; index < pass; index++) {
-        numComparisons++;
-        if(data.get(index).compareTo( data.get(maxPos)  ) > 0) {
+
+        numComparisons++;   // Each time a for loop is run, there's a comparison being made        if(data.get(index).compareTo( data.get(maxPos)  ) > 0) {
           maxPos = index;
         }
         numComparisons++;
@@ -74,17 +79,19 @@ public class Sorts{
       data.add(pass, data.get(maxPos));
       data.remove(data.get(maxPos));
       numSwaps++;
-      
+
       System.out.println( "after swap: " +  data );//diag
-      System.out.println("Number of comparisons: " + numComparisons);
-      System.out.println("Number of swaps: " + numSwaps);
     }
+    System.out.println("# of Comparisons: " + numComparisons);
+    System.out.println("# of Swaps: " + numSwaps);
     return data;
   }
 
   public static ArrayList<Comparable> insertion (ArrayList<Comparable> input ){
+    numComparisons = 0; numSwaps = 0;
    ArrayList<Comparable> data = input;
    for(int partition = 0; partition < data.size() - 1; partition++  ) {
+     numComparisons++;   // Each time a for loop is run, there's a comparison being made
      //partition marks first item in unsorted region
 
      System.out.println( "\npartition: " + partition + "\tdataset:"); //diag
@@ -92,24 +99,26 @@ public class Sorts{
 
      //traverse sorted region from right to left
      for(int j = partition + 1; j > 0; j--) {
-
+       numComparisons++;
        // "walk" the current item to where it belongs
        // by swapping adjacent items
        if ( data.get(j).compareTo( data.get(j - 1) ) < 0  ) {
 
          System.out.println( "swap indices "+ (j) +" & "+ (j - 1) +"..." ); //diag
-         
+
          Comparable temp = data.get(j);
          data.set(j, data.get(j - 1));
          data.set(j - 1, temp);
-         
+         numSwaps++;
        }
        else
-          
+
          break;
      }
    }
+   System.out.println("# of Comparisons: " + numComparisons);
+   System.out.println("# of Swaps: " + numSwaps);
    return data;
  }
- 
+
 }
